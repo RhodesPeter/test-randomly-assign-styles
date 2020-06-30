23455b5d537569ff19a62d62aec9cdba7ddba075
Peter Rhodes <rhodespeter@hotmail.co.uk>
2020-06-30 21:31:27 +0100

    Add minified version

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-(function () {
-  const pickFont = () => ['SS01', 'SS02', 'SS03', 'SS04'][Math.floor(Math.random() * 4)];
-  const elements = document.body.querySelectorAll(
-    'div, p, h1, h2, h3, h4, h5, h6, li, span, button, a'
-  );
+// ----- ES5 -----
+// "use strict";
+// (function () {
+//   var pickFont = function pickFont() {
+//     return ['SS01', 'SS02', 'SS03', 'SS04'][Math.floor(Math.random() * 4)];
+//   };
 
-  const styleText = (el) => {
-    el.innerHTML = el.textContent
-      .split('')
-      .map((letter) =>
-        letter.match(/[A-Z0-9]|[^A-Za-z0-9 ]/g)
-          ? `<span style="font-family:${pickFont()};">${letter}</span>`
-          : letter
-      )
-      .join('');
-  };
+//   var elements = document.body.querySelectorAll('div, p, h1, h2, h3, h4, h5, h6, li, span, button, a');
 
-  elements.forEach(styleText);
-})();
+//   var styleText = function styleText(el) {
+//     el.innerHTML = el.textContent.split('').map(function (letter) {
+//       return letter.match(/[A-Z0-9]|[^A-Za-z0-9 ]/g) ? "<span style=\"font-family:".concat(pickFont(), ";\">").concat(letter, "</span>") : letter;
+//     }).join('');
+//   };
+
+//   elements.forEach(styleText);
+// })();
+
+// ----- ES6 -----
+// (function () {
+//   const pickFont = () => ['SS01', 'SS02', 'SS03', 'SS04'][Math.floor(Math.random() * 4)];
+//   const elements = document.body.querySelectorAll(
+//     'div, p, h1, h2, h3, h4, h5, h6, li, span, button, a'
+//   );
+
+//   const styleText = (el) => {
+//     el.innerHTML = el.textContent
+//       .split('')
+//       .map((letter) =>
+//         letter.match(/[A-Z0-9]|[^A-Za-z0-9 ]/g)
+//           ? `<span style="font-family:${pickFont()};">${letter}</span>`
+//           : letter
+//       )
+//       .join('');
+//   };
+
+//   elements.forEach(styleText);
+// })();
 
 
-// minified
-// document.body.querySelectorAll("div, p, h1, h2, h3, h4, h5, h6, li, span, button, a").forEach(t=>{t.innerHTML=t.textContent.split("").map(t=>t.match(/[A-Z0-9]|[^A-Za-z0-9 ]/g)?`<span style="font-family:${(()=>["SS01","SS02","SS03","SS04"][Math.floor(4*Math.random())])()};">${t}</span>`:t).join("")});
+// ----- ES5 minified -----
+(function(){var a=function(){return["SS01","SS02","SS03","SS04"][Math.floor(4*Math.random())]},b=document.body.querySelectorAll("div, p, h1, h2, h3, h4, h5, h6, li, span, button, a");b.forEach(function(b){b.innerHTML=b.textContent.split("").map(function(b){return b.match(/[A-Z0-9]|[^A-Za-z0-9 ]/g)?"<span style=\"font-family:".concat(a(),";\">").concat(b,"</span>"):b}).join("")})})();
